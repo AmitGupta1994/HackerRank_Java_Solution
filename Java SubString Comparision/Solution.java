f33b96cb19d33ce1a4d50dde72a2b414506eb991
Amit Gupta <iamitgupta1994@gmail.com>
2020-04-16 11:45:22 +0545

    [add][master]: calculates smallest substring in Java SubString Comparision problem

--- Java SubString Comparision/Solution.java
+++ Java SubString Comparision/Solution.java
@@ -8,6 +8,27 @@ public class Solution {
         int k = scan.nextInt();
         scan.close();
       
-        //System.out.println(getSmallestAndLargest(s, k));
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+
+    public static String getSmallestAndLargest(String s, int k) {
+    	String smallest = "";
+        
+        int numberOfSubStr = s.length() - (k-1) ;
+
+        String[] subStrArray = new String[numberOfSubStr];
+        for (int i=0;i<numberOfSubStr;i++){
+            subStrArray[i] = s.substring(i,i+k);
+        }
+
+        smallest = subStrArray[0];
+        for (int i=1;i<subStrArray.length;i++){
+            if(smallest.compareTo(subStrArray[i])>=0){
+                smallest = subStrArray[i];
+            }
+            
+        }
+
+        return smallest;
     }
 }
