fb3d68a132577abf0eefc3b0ba09976ef5fdf3f1
Amit Gupta <iamitgupta1994@gmail.com>
2020-04-16 11:42:04 +0545

    [add][master]: adds SOlution.java for Java STring Comparision

---
+++ Java SubString Comparision/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        //System.out.println(getSmallestAndLargest(s, k));
+    }
+}
