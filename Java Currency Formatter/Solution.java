b256a0c561cb9025fa08caf597f0ffdf6fbcf068
Amit Gupta <iamitgupta1994@gmail.com>
2020-04-17 10:46:01 +0545

    [add][master]: adds currency formatter solution for US,China and Franece

---
+++ Java Currency Formatter/Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        NumberFormat nfUS = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = nfUS.format(payment);
+        
+        NumberFormat nfChina = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = nfChina.format(payment);
+
+        NumberFormat nfFrance = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = nfFrance.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
