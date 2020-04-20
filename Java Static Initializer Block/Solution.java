2ac013f99e528f61cfe7c3f035d5abb275c082f0
Amit Gupta <iamitgupta1994@gmail.com>
2020-04-20 15:16:26 +0545

    [adds][master]: completes Java Static Initializer block

--- Java Static Initializer Block/Solution.java
+++ Java Static Initializer Block/Solution.java
@@ -5,6 +5,23 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
+
+	public static boolean flag = initializeFlag();
+	public static int B;
+	public static int H;
+
+	public static boolean initializeFlag(){
+    Scanner scanner = new Scanner(System.in);
+    B= scanner.nextInt();
+    H= scanner.nextInt();
+    if(B>0 && H>0){
+        return true;
+    }else {
+        System.out.print("java.lang.Exception: Breadth and height must be positive");
+        return false;
+    }
+}
+
 	public static void main(String[] args){
 		if(flag){
 			int area=B*H;
